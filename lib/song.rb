a082262b48c264b251c7aabf72678fd2a8c103f4
meaghan jennings <meaghanljennings@gmail.com>
2020-10-21 19:38:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,11 @@ class Song
   attr_reader :artist
 
   @@songs = []
-
+  
+  def initialize
+    self.class.all << self
+  end
+  
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
